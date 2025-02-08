4e224dde3b904ba9424f6791ceab523d0d3f49bd
ShaoYu <shaoyukao@cht.com.tw>
2025-02-08 16:28:42 +0800

    重構前後台佈局，新增導航欄與頁腳，整合路由連結

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,14 +1,52 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 function AdminLayout() {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <div>
-      <header>AdminLayout Header</header>
+      <header className="p-3">
+        <div className="container">
+
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <label className="navbar-brandv h2">後台</label>
+
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link to="/admin/products" className={`nav-link px-2 ${pathname === '/admin/products' ? 'active' : ''}`}>產品管理</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/admin/orders" className={`nav-link px-2 ${pathname === '/admin/orders' ? 'active' : ''}`}>訂單管理</Link>
+                  </li>
+                </ul>
+
+                <form className="d-flex">
+                  <button className="btn btn-outline-success" type="button">登出</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+
       <main>
-        <h1>AdminLayout Layout</h1>
-        <Outlet />
+        <div className="container">
+          <Outlet />
+        </div>
       </main>
-      <footer>AdminLayout Footer</footer>
+      
+      <footer>
+        <div className="container">
+          <p>© 2024 React 作品實戰冬季班 第六週</p>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/layouts/FrontendLayout.jsx
+++ src/layouts/FrontendLayout.jsx
@@ -1,14 +1,53 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 function FrontendLayout() {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <div>
-      <header>Frontend Header</header>
+      <header className="p-3">
+        <div className="container">
+
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              {/* <a className="navbar-brand" href="#">Navbar</a> */}
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link to="/" className={`nav-link px-2 ${pathname === '/' ? 'active' : ''}`}>首頁</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/product" className={`nav-link px-2 ${pathname === '/product' ? 'active' : ''}`}>產品</Link>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <Link to="/cart" className={`nav-link px-2 ${pathname === '/cart' ? 'active' : ''}`}>購物車</Link>
+                  </li>
+                </ul>
+                <form className="d-flex">
+                  <Link to="/login" className="btn btn-outline-primary me-2">登入</Link>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+        </div>
+      </header>
+      
       <main>
-        <h1>Frontend Layout</h1>
-        <Outlet />
+        <div className="container">
+          <Outlet />
+        </div>
       </main>
-      <footer>Frontend Footer</footer>
+      
+      <footer>
+        <div className="container">
+          <p>© 2024 React 作品實戰冬季班 第六週</p>
+        </div>
+      </footer>
     </div>
   );
 }

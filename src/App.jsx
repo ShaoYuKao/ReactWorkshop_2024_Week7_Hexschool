1ec412dc332c322459a362d689157a250379f340
ShaoYu <shaoyukao@cht.com.tw>
2025-02-08 14:26:39 +0800

    新增 Bootstrap 依賴，並在 App 組件中整合 Bootstrap 樣式與元件

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      
+      <div className="container">
+        <h1>Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click me</button>
+      </div>
     </>
   )
 }

27148b6ffac6b75f30d7262d2631a60a0a319de4
ShaoYu <shaoyu.kao@gmail.com>
2025-02-09 14:33:26 +0800

    style(layout): 更新 AdminLayout 組件的結構與樣式

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -5,7 +5,7 @@ function AdminLayout() {
   const { pathname } = location;
 
   return (
-    <div>
+    <>
       <header className="p-3">
         <div className="container">
 
@@ -36,18 +36,18 @@ function AdminLayout() {
         </div>
       </header>
 
-      <main>
+      <main className="content">
         <div className="container">
           <Outlet />
         </div>
       </main>
       
-      <footer>
+      <footer className="bg-light text-center py-3">
         <div className="container">
-          <p>© 2024 React 作品實戰冬季班 第六週</p>
+          <p className="text-center">© 2024 React 作品實戰冬季班 第六週</p>
         </div>
       </footer>
-    </div>
+    </>
   );
 }
 
